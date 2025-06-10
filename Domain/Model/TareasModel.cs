14ce8ba14e8c7337e84b4a1dbd006328315e1f7a
SOFYAPALMA <SOFY4.PALMA@GMAIL.COM>
2025-06-10 18:58:00 -0500

    Creacion de modelos, Capa que representa las entidades de la BD

---
+++ Domain/Model/TareasModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Model
+{
+    public class TareasModel
+    {
+       public int IdTarea { get; set; }
+       public string Actividad { get; set; }
+       public bool Estado { get; set; }
+       public DateTime FechaApertura { get; set; }
+       public DateTime FechaCierre{ get; set; }
+
+    }
+}
---
+++ Domain/Model/UsuarioModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Model
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+
+    }
+}
