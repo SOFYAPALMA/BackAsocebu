397257829d648aa4ea64623556c7c65b53254807
SOFYAPALMA <SOFY4.PALMA@GMAIL.COM>
2025-06-10 22:29:40 -0500

    Creacion de controladores para solicitudes del usuario, interaccion entre visstas y capas de negocio

---
+++ To_Do API/Controllers/TareasController.cs
@@ -0,0 +1,134 @@
+﻿using Business.IService;
+using Commun;
+using Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace To_Do_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TareasController : ControllerBase
+    {
+        private readonly ITareasService _tareasS;
+
+        public TareasController(ITareasService tareasS)
+        {
+           this._tareasS = tareasS;
+        }
+
+        [HttpPost]
+        [Route("CrearTarea")]
+
+        public async Task<Result> CrearTarea([FromBody] TareasCrearDto tareasCrear)
+        {
+            try
+            {
+                return await _tareasS.CrearTarea(tareasCrear);
+            }
+            catch (Exception ex)
+            {
+                Result oRespuesta = new();
+                oRespuesta.Success = false;
+                oRespuesta.Message = ex.Message + " - Inner: " + ex.InnerException;
+
+                return oRespuesta;
+            }
+        }
+
+        [HttpGet]
+        [Route("ConsultarTareas")]
+        public async Task<Result> ConsultarTareas()
+        {
+            Result oRespuesta = new();
+
+            try
+            {
+                var vResult = await _tareasS.ConsultarTareas();
+
+                oRespuesta.Success = vResult.Success;
+                oRespuesta.Message = vResult.Message;
+                oRespuesta.Data = vResult.Data;
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Success = false;
+                oRespuesta.Message = ex.Message + " - Inner: " + ex.InnerException;
+
+            }
+            return (oRespuesta);
+        }
+
+        [HttpGet]
+        [Route("ConsultarTareaId")]
+        public async Task<Result> ConsultarTareaId(int id)
+        {
+            Result oRespuesta = new();
+
+            try
+            {
+                var vResult = await _tareasS.ConsultarTareaId(id);
+
+                oRespuesta.Success = vResult.Success;
+                oRespuesta.Message = vResult.Message;
+                oRespuesta.Data = vResult.Data;
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Success = false;
+                oRespuesta.Message = ex.Message + " - Inner: " + ex.InnerException;
+
+            }
+            return (oRespuesta);
+        }
+
+        [HttpPut]
+        [Route("ActualizarTarea")]
+        public async Task<IActionResult> ActualizarTarea([FromBody] TareasDto objModel)
+        {
+            Result oRespuesta = new();
+
+            try
+            {
+                var vRespuesta = await _tareasS.ActualizarTarea(objModel);
+
+                oRespuesta.Success = vRespuesta.Success;
+                oRespuesta.Message = vRespuesta.Message;
+                oRespuesta.Data = vRespuesta.Data;
+            }
+            catch (Exception ex)
+            {
+
+                oRespuesta.Success = false;
+                oRespuesta.Message = ex.Message + " - Inner: " + ex.InnerException;
+            }
+            return Ok(oRespuesta);
+        }
+
+        [HttpDelete]
+        [Route("EliminarTareaId")]
+        public async Task<Result> EliminarTareaId(int id)
+        {
+            Result oRespuesta = new();
+            try
+            {
+                var vRespuesta = await _tareasS.EliminarTareaId(id);
+
+                oRespuesta.Success = vRespuesta.Success;
+                oRespuesta.Message = vRespuesta.Message;
+                oRespuesta.Data = vRespuesta.Data;
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Success = false;
+                oRespuesta.Message = ex.Message + " - Inner: " + ex.InnerException;
+
+            }
+            return (oRespuesta);
+        }
+
+
+    }
+}
+
+
+
---
+++ To_Do API/Controllers/UsuarioController.cs
@@ -0,0 +1,90 @@
+﻿using Business.IService;
+using Business.Service;
+using Commun;
+using Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace To_Do_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioService _usuarioS;
+
+        public UsuarioController(IUsuarioService usuarioS)
+        {
+            this._usuarioS = usuarioS;
+        }
+
+        [HttpPost]
+        [Route("CrearUsuario")]
+
+        public async Task<Result> CrearUsuario([FromBody] UsuarioCrearDto usuarioCrear)
+        {
+            try
+            {
+                return await _usuarioS.CrearUsuario(usuarioCrear);
+            }
+            catch (Exception ex)
+            {
+                Result oRespuesta = new();
+                oRespuesta.Success = false;
+                oRespuesta.Message = ex.Message + " - Inner: " + ex.InnerException;
+
+                return oRespuesta;
+            }
+        }
+
+
+        [HttpGet]
+        [Route("ConsultarUsuarioEmail")]
+        public async Task<Result> GetUsuariosemail(string correo)
+        {
+            Result oRespuesta = new();
+
+            try
+            {
+                var vResult = await _usuarioS.ConsultarUsuarioEmail(correo);
+
+                oRespuesta.Success = vResult.Success;
+                oRespuesta.Message = vResult.Message;
+                oRespuesta.Data = vResult.Data;
+            }
+            catch (Exception ex)
+            {
+                oRespuesta.Success = false;
+                oRespuesta.Message = ex.Message + " - Inner: " + ex.InnerException;
+
+            }
+            return (oRespuesta);
+
+        }
+
+
+        [HttpPost]
+        [Route("ValidarLogin")]
+        [AllowAnonymous]
+        public async Task<Result> ValidarLogin([FromBody] IniciarSesionDto loginModel)
+        {
+            Result oRespuesta = new();
+
+            try
+            {
+                var vRespuesta = await _usuarioS.ValidarLogin(loginModel);
+
+                oRespuesta.Success = vRespuesta.Success;
+                oRespuesta.Message = vRespuesta.Message;
+                oRespuesta.Data = vRespuesta.Data;
+            }
+            catch (Exception ex)
+            {
+                
+                oRespuesta.Success = false;
+                oRespuesta.Message = ex.Message + " - Inner: " + ex.InnerException;
+            }
+            return oRespuesta;
+        }
+    }
+}
