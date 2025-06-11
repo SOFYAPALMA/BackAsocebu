94f2b28b35bbd1ad538e02e0657f90f20c1e4083
SOFYAPALMA <SOFY4.PALMA@GMAIL.COM>
2025-06-10 20:10:50 -0500

    creacion de DBContext, conexion entre la BD y la API

---
+++ Repository/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(equals => equals.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+            base.OnModelCreating(modelBuilder);
+        }
+        
+        public DbSet<UsuarioModel> Usuario { get; set; }
+        public DbSet<TareasModel> Libros { get; set; }
+       
+
+    }
+}
